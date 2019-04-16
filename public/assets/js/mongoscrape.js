bef25598d16c16298535a34d6cfadf62fc07cf11
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-04-16 15:44:54 -0400

    notes and delete button added

--- public/assets/js/mongoscrape.js
+++ public/assets/js/mongoscrape.js
@@ -4,49 +4,7 @@ $(function() {
     
     $('#display').on('click', function (event) {
         event.preventDefault();
-        $("#scrapenews").empty();
-        // Grab all of the scraped news
-        $.getJSON("/api/news", function(data) {
-            if(data.length>0){
-                // For each news...
-                for (var i = 0; i < data.length; i++) {  
-                    // ...populate #results with a p-tag that includes the note's title and object id
-                        $("#scrapenews").prepend(
-                            `<div class="card bg-light mb-3" data-id=${data[i]._id}">
-                                <div class="card-header">${data[i].headline}</div>
-                                <div class="card-body">
-                                    <h5 class="card-title"><a href="https://www.nytimes.com/section/us${data[i].url}">https://www.nytimes.com/section/us${data[i].url}</a></h5>
-                                    <p class="card-text">${data[i].summary}</p>
-                                    <form class="form-inline my-2 my-lg-0">
-                                    <button class="btn btn-md btn-success font-weight-bold my-2 my-sm-0 mr-2 saveArticle" data-id=${data[i]._id}>Save Article!</button>
-                                    </form>
-                                </div>
-                            </div>`
-                        );
-                        $('.saveArticle').unbind().click(function(event) {
-                            event.preventDefault();
-                            articleid=$(this).attr("data-id");       
-                            $.ajax({
-                                type: "POST",
-                                dataType: "json",
-                                url: "/",
-                                data: {
-                                  save: 'Yes',
-                                  articleid: articleid
-                                }
-                              })
-                              // If that API call succeeds, add the title and a delete button for the note to the page
-                            .then(function(data) {  
-                            });
-                            $('#savemsg').modal('show');
-                        });
-        
-                }
-            }else{
-                $("#scrapenews").html("<p id='bare'>mongo scraper</p>");
-            }
-            
-        });
+        displaynews("/api/news")
     });  
     
     $('#scrape').on('click', function (event) {
@@ -68,32 +26,62 @@ $(function() {
 
     $('#savednews').on('click', function (event) {
         event.preventDefault();
+        displaynews("/api/savednews")
+    }); 
+
+    function displaynews(newsapi){
         $("#scrapenews").empty();
         // Grab all of the scraped news
-        $.getJSON("/api/savednews", function(data) {
+        $.getJSON(`${newsapi}`, function(data) {
             if(data.length>0){
+                let buttondisp;
                 // For each news...
                 for (var i = 0; i < data.length; i++) {  
+                    if(newsapi==="/api/news"){
+                        buttondisp=`<button class="btn btn-md btn-success font-weight-bold my-2 my-sm-0 mr-2 saveArticle" data-id=${data[i]._id}>Save Article!</button>`;
+                    }else if(newsapi==="/api/savednews"){
+                        buttondisp=`<button class="btn btn-md btn-info font-weight-bold my-2 my-sm-0 mr-2 addNotes" data-id=${data[i]._id}>Add Notes</button>
+                                    <button class="btn btn-md btn-danger font-weight-bold my-2 my-sm-0 mr-2 deletesaved" data-id=${data[i]._id}>Delete from saved</button>`;
+                    }
                     // ...populate #results with a p-tag that includes the note's title and object id
-                        $("#scrapenews").prepend(
-                            `<div class="card bg-light mb-3" data-id=${data[i]._id}">
-                                <div class="card-header">${data[i].headline}</div>
-                                <div class="card-body">
-                                    <h5 class="card-title"><a href="https://www.nytimes.com/section/us${data[i].url}">https://www.nytimes.com/section/us${data[i].url}</a></h5>
-                                    <p class="card-text">${data[i].summary}</p>
-                                    <form class="form-inline my-2 my-lg-0">
-                                    <button class="btn btn-md btn-success font-weight-bold my-2 my-sm-0 mr-2 notes" data-id=${data[i]._id}>Save Article!</button>
-                                    </form>
-                                </div>
-                            </div>`
-                        );
-       
+                    $("#scrapenews").prepend(
+                        `<div class="card bg-light mb-3" data-id=${data[i]._id}">
+                            <div class="card-header">${data[i].headline}</div>
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="https://www.nytimes.com${data[i].url}" target="blank">https://www.nytimes.com${data[i].url}</a></h5>
+                                <p class="card-text">${data[i].summary}</p>
+                                <form class="form-inline my-2 my-lg-0">
+                                ${buttondisp}
+                                </form>
+                            </div>
+                        </div>`
+                    );
+                    if(newsapi==="/api/news"){
+                        $('.saveArticle').unbind().click(function(event) {
+                            event.preventDefault();
+                            articleid=$(this).attr("data-id");       
+                            $.ajax({
+                                type: "POST",
+                                dataType: "json",
+                                url: "/",
+                                data: {
+                                save: 'Yes',
+                                articleid: articleid
+                                }
+                            })
+                            // If that API call succeeds, add the title and a delete button for the note to the page
+                            .then(function(data) {  
+                            });
+                            $('#savemsg').modal('show');
+                        });
+                    }
                 }
             }else{
                 $("#scrapenews").html("<p id='bare'>mongo scraper</p>");
             }
             
         });
-    }); 
+
+    }
 
 });
