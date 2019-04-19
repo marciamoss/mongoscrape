df8b29f7968cd9d2ebe0d052eadd30afa4a164f7
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-04-19 18:19:28 -0400

    heroku db added

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect('mongodb://localhost/newsscraper', { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Import routes and give the server access to them.
 app.use(require('./routes/htmlRoutes')(db));
